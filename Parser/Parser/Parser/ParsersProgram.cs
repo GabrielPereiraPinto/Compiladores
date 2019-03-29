dfaff7eff7494c336bbda6a74232cde257627d9f
GabrielPereiraPinto <arkanjogabriel7@hotmail.com>
2019-03-28 23:55:00 -0300

    Parser01

--- Parser/Parser/Parser/ParsersProgram.cs
+++ Parser/Parser/Parser/ParsersProgram.cs
@@ -10,18 +10,42 @@ namespace Parser
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Imprimindo S -> SS+ | SS- | a \nDigitar valores desejados \nPara Teste utilizar 5-4 com saida 54- ou 7+8 com saida 78+");
-            //var parsepos = new ParserPos();
-            //parsepos.expr();
-            //Console.Write("\n\n");
-            //Console.In.Read();
-            //Console.WriteLine("Imprimindo S -> +SS | -SS | a \nDigitar valores desejados \nPara Teste utilizar 1-3 com saida -13 ou 4+5 com saida +45");
-            //var parserPre = new ParserPre();
-            //Console.Write("\n\n");
-            //Console.In.Read();
-            Console.WriteLine("Imprimindo S ->S(S)S | E\nDigitar valores desejados");
-            var parseParenthesis = new ParserParenthesis();
-            parseParenthesis.expr();
+            Console.WriteLine("Digite o valor para o Parser desejado\n1- Para o Parser Posfixado\n2- Para o Parser Prefixado\n3- Para o Parser Parenthesis\n4- Para o Parser 01\n5- Para fechar o programa\n\n");
+            string menu = Console.ReadLine();
+            switch (menu)
+            {
+                case "1":
+                    Console.WriteLine("Imprimindo S -> SS+ | SS- | a \nDigitar valores desejados \nPara Teste utilizar 5-4 com saida 54- ou 7+8 com saida 78+");
+                    var parsepos = new ParserPos();
+                    parsepos.expr();
+                    Console.Write("\n\n");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Imprimindo S -> +SS | -SS | a \nDigitar valores desejados \nPara Teste utilizar 1-3 com saida -13 ou 4+5 com saida +45");
+                    var parserPre = new ParserPre();
+                    parserPre.expr();
+                    Console.Write("\n\n");
+                    break;
+
+                case "3":
+                    Console.WriteLine("Imprimindo S ->S(S)S | E\nDigitar valores desejados");
+                    var parseParenthesis = new ParserParenthesis();
+                    parseParenthesis.expr();
+                    break;
+
+                case "4":
+                    Console.WriteLine("Imprimindo S ->0S1 | E\nDigitar valores desejados");
+                    var parse01 = new Parser01();
+                    parse01.expr();
+                    break;
+                
+                case "5":
+                    return;
+            }
+            Console.WriteLine("Aperte Enter para sair");
+            Console.ReadLine();
+            Console.ReadLine();
         }
     }
 
@@ -64,7 +88,7 @@ namespace Parser
                 {
                     match('+');
                     term();
-                    Console.Write('-');
+                    Console.Write('+');
                 }
                 else if (lookAhead == '-')
                 {
@@ -72,6 +96,8 @@ namespace Parser
                     term();
                     Console.Write('-');
                 }
+                else
+                    return;
             }
         }
     }
@@ -159,7 +185,59 @@ namespace Parser
                     default:
                         if ((lookAhead == 13 && counter == 0) || lookAhead == ' ' && counter == 0) // 13 é o valor de enter em ASCII
                         {
-                            Console.Write('\n');
+                            Console.Write("\nLinguagem valida\n");
+                        }
+                        else
+                            throw new Exception("Syntax Error");
+
+                        return;
+                }
+            }
+        }
+    }
+
+    public class Parser01 : Parser
+    {
+        public int counter;
+        public bool flag = false;
+        public Parser01()
+        {
+            lookAhead = Console.In.Read();
+        }
+
+        public void term()
+        {
+            if ('0' == (char)lookAhead && flag == false)
+            {
+                counter++;
+                Console.Write((char)lookAhead);
+                match(lookAhead);
+            }
+            else if ((char)lookAhead == '1' && counter > 0)
+            {
+                flag = true;
+                counter--;
+                Console.Write((char)lookAhead);
+                match(lookAhead);
+            }
+            else
+                throw new Exception("Syntax Error");
+        }
+
+        public override void expr()
+        {
+            while (true)
+            {
+                switch (lookAhead)
+                {
+                    case '0':
+                    case '1':
+                        term();
+                        break;
+                    default:
+                        if ((lookAhead == 13 && counter == 0 && flag == true) || lookAhead == ' ' && counter == 0 && flag == true) // 13 é o valor de enter em ASCII
+                        {
+                            Console.Write("\nLinguagem valida\n");
                         }
                         else
                             throw new Exception("Syntax Error");
